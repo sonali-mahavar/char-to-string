a597cd97e58dd493ee94d046222ab0ea3db55540
sonali mahavar <53106053+sonali-mahavar@users.noreply.github.com>
2019-12-25 10:52:23 +0530

    Add files via upload

---
+++ Chartostring.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chartostring;
+import java.util.Scanner;
+public class Chartostring {
+    public static void main(String[] args) {
+        char ch='A';
+        String s=String.valueOf(ch);
+        System.out.println("The string is:" +s);
+    }
+    
+}
